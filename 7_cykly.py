3e296c62656bed0f3025bca39bc5916aae31922b
Andrea Waltlova <awaltlov@redhat.com>
2022-01-30 12:51:31 +0100

    Add all solutions

--- 7_cykly.py
+++ 7_cykly.py
@@ -1,6 +1,5 @@
 # 1 Seznam hodnocení
 # Mějme seznam hodnocení divadelní hry Plyšáci na útěku, který vypadá takto:
-
 hodnoceni = [7, 9, 6, 7, 9, 8]
 # Vytvořte program, který projde tento seznam a vypíše každé hodnocení na nový řádek.
 # Upravte program tak, aby vypisoval výstup v tomto formátu
@@ -10,6 +9,10 @@ hodnoceni = [7, 9, 6, 7, 9, 8]
 # 7/10
 # 9/10
 # 8/10
+
+for prvek in hodnoceni:
+    print(f"{prvek}/10")
+
 
 # 2 Procházení seznamu
 # Založte si program hesla.py a na jeho začátek vložte následující kód obsahující seznam hesel pro přihlášení do nějakého systému
@@ -28,8 +31,16 @@ hesla = [
     "strčprstskrzkrk",
 ]
 # Pomocí cyklu vypište všechny hesla na obrazovku, každé na jeden řádek.
+for heslo in hesla:
+    print(heslo)
 # Upravte váš program tak, aby vypisoval jen bezpečná hesla, tedy taková, jež jsou delší než 8 znaků.
+for heslo in hesla:
+    if len(heslo) > 8:
+        print(heslo)
 # Upravte váš program tak, aby vypisoval jen ta hesla, jež obsahují znak pomlčky ‘-’.
+for heslo in hesla:
+    if "-" in heslo:
+        print(heslo)
 
 # 3 Složitější seznam
 # Založte si program cykly02.py a použijte v něm následující seznam měsíců v roce, Všimněte si, že je to seznam seznamů.
@@ -49,8 +60,11 @@ mesice = [
     ["prosinec", 31],
 ]
 # Pomocí cyklu projděte tento seznam a vypište na výstup názvy jednotlivých měsíců.
+for mesic in mesice:
+    print(mesic[0])
 # Pomocí dalšího cyklu vypište na výstup počty dní v jednotlivých měsících.
-
+for mesic in mesice:
+    print(mesic[1])
 
 # Bonusy
 
@@ -73,8 +87,32 @@ hry = [
     ["Smrt v přímém přenosu", 265],
 ]
 # Pomocí cyklu projděte tento seznam a vypište na výstup názvy všech her.
+for hra in hry:
+    print(hra[0])
 # Vypište na výstup názvy všech her, které trvají více než 120 minut.
+for hra in hry:
+    if hra[1] > 120:
+        print(hra[0])
 # Vypište na výstup názvy všech her spolu s jejich trváním v hodinách a minutách.
+for hra in hry:
+    print(f"{hra[0]} trvá {hra[1] // 60} hodin a {hra[1] % 60} minut.")
+    print(hra[0] + " trvá " + str(hra[1] // 60) +
+          " hodin a " + str(hra[1] % 60) + " minut.")
 
 # 5 Průměr
 # Napište cyklus, který projde zadaný seznam desetinných čísel a spočítá jejich průměr. Seznam čísel si vytvořte na začátku programu.
+cisla = [10.5, 13.3, 17.2, 11.5]
+soucet = 0
+for cislo in cisla:
+    soucet += cislo
+prumer = soucet / len(cisla)
+print(prumer)
+
+# 6 Největší prvek
+# Napište cyklus, který projde zadaný seznam celých čísel a najde v něm největší hodnotu.
+cisla = [11, 23, 45, 1, 9, 65, 34]
+nejvetsi = cisla[0]
+for cislo in cisla:
+    if cislo > nejvetsi:
+        nejvetsi = cislo
+print(nejvetsi)
